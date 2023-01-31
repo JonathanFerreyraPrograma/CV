01302cf6a424be8f723f284616f635323d8f088a
JonathanFerreyraPrograma <124083806+JonathanFerreyraPrograma@users.noreply.github.com>
2023-01-31 17:27:11 -0300

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,14 @@
+document.getElementById("secundaria").onclick = function () {
+    console.log("Vamos al link")
+    window.location.href="https://oficinatecnicaet11.wixsite.com/et11manuelbelgrano"
+}
+
+document.getElementById("policiaf").onclick = function () {
+    console.log("Vamos al link")
+    window.location.href="https://www.iupfa.edu.ar/suboficiales/"
+}
+
+document.getElementById("facultad").onclick = function () {
+    console.log("Vamos al link")
+    window.location.href="https://www.universidad-policial.edu.ar/"
+}
